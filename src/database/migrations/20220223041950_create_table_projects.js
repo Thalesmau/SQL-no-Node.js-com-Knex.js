942d1032fe7ce637b824e1e5849217e45c2f88de
Thalesmau <thalesmausiq46@gmail.com>
2022-02-23 02:02:43 -0300

    paginação de projetos

---
+++ src/database/migrations/20220223041950_create_table_projects.js
@@ -0,0 +1,16 @@
+exports.up = knex => knex.schema.createTable('projects', table => {
+    table.increments('id')
+    table.text('title')
+
+    //relacionamento
+
+    table.integer('user_id')
+        .references('users.id')
+        .notNullable()
+        .onDelete('CASCADE')
+
+    table.timestamp('create_at').defaultTo(knex.fn.now())
+    table.timestamp('uptade_at').defaultTo(knex.fn.now())
+});
+
+exports.down = knex => knex.schema.dropTable('projects')
---
+++ src/database/seeds/002_projects.js
@@ -0,0 +1,13 @@
+
+ exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('projects').del()
+  .then(function () {
+    return knex('projects').insert([
+      {
+        user_id: 1,
+        title: "Meu projeto"
+      }
+    ]);
+  });
+}
